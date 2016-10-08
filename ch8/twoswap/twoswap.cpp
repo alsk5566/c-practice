fd19b12cdfe8d4a2f8967aca98b1aa01ddedc1ba
alsk5566 <alsk5566@gmail.com>
2016-10-08 23:43:53 +0800

    ch8

---
+++ ch8/twoswap/twoswap.cpp
@@ -0,0 +1,25 @@
+// twoswap.cpp : 定義主控台應用程式的進入點。
+//
+
+#include "stdafx.h"
+#include<iostream>
+
+template<typename T>
+void Swap(T&a, T&b);
+
+struct  job
+{
+	char name[40];
+	double salary;
+	int floor;
+};
+
+template<>void Swap<job>(job &j1, job&j2);
+
+
+
+int main()
+{
+    return 0;
+}
+
